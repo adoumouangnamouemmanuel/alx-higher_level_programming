8bd2fcebb9516e515711251259724f1b7ef7ee8b
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-01-29 11:08:32 -0800

    alx project

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+def Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0; height=0):
+        """Initialize the rectangle object
+
+        args:
+            width(int): width of the rectangle
+            height(int): height of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        """get/set the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not instance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get/set the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not instance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
