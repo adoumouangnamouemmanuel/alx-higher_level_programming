6c8031385169008362d1b17ac77b054f008632dc
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-01-29 11:14:27 -0800

    alx project

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represents a rectangle"""
 
-    def __init__(self, width=0; height=0):
+    def __init__(self, width=0, height=0):
         """Initialize the rectangle object
 
         args:
