40da540fd834a7087f9af8589d4961be7e4e59c6
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-02 11:25:23 -0800

    Alx project

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -25,3 +25,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """compute the area of the square"""
+        return (self.__size * self.__size)
