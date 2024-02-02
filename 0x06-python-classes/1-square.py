9394091e4de1c4a95d543741e3b997712d0dd13e
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-02 10:59:15 -0800

    Alx project

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+
+        Args:
+            size(int): the size of the new square
+        """
+        self.__size = size
