8d58ddda7d77afb4e4225dc210c2f3b95283b05a
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-02 11:23:38 -0800

    Alx project

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines the Square class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+            size(int): the size of the new square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get/set the size of the square"""
+        return self.__size
+
+    @setter.size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
