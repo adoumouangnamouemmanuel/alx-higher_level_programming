982030fda5355c47e647b3e8a1b26a88cc2d4b47
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-02 11:30:59 -0800

    Alx project

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Defines the Square class"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square
+        """Initialize a new square.
 
         Args:
-            size(int): the size of the new square
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """get/set the size of the square"""
+        """Get/set the current size of the square."""
         return (self.__size)
 
-    @setter.size
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -27,5 +28,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """compute the area of the square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
