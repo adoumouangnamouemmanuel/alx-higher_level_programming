47b190323efb415fdda7e4b602e55898c9a68b98
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-02 11:11:56 -0800

    Alx project

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,8 +11,8 @@ class Square:
         Args:
             size(int): the size of the new square
         """
-        self.__size = size
-        if isinstance(self.__size, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
