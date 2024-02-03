c782db3969f757efbca53d48b716aa3e3ca18e99
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-03 07:27:04 -0800

    Alx project

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Print the alphabet in lowercase, not followed by a new line."""
 
-for i in range(65, 123):
-    print("{}".format(chr(i)), end="")
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
