7f302a2ce13d8138059d0208bd2476917aeb0363
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-03 07:36:43 -0800

    Alx project

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 
 for letter in range(97, 123):
-    if chr(letter) != "q" and chr(letter) != "q":
+    if chr(letter) != "q" and chr(letter) != "e":
         print("{}".format(chr(letter)), end="")
