86ba394761b341dc19c7a6fdbf4f01c64f424573
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-05 23:59:17 -0800

    alx project

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines lookup function"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
