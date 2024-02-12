f48f25078c9d9e1566cccc2660059e05931ce209
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2024-02-12 15:08:39 -0800

    alx project

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,31 +40,35 @@ class Square(Rectangle):
         self.__height = value
 
     def update(self, *args, **kwargs):
-        """Updates attributes of an instance"""
-
-        if args is not None and len(args) != 0:
-            if len(args) >= 1:
-                if type(args[0]) != int and args[0] is not None:
-                    raise TypeError("id must be an integer")
-                self.id = args[0]
-            if len(args) > 1:
-                self.size = args[1]
-            if len(args) > 2:
-                self.x = args[2]
-            if len(args) > 3:
-                self.y = args[3]
-        else:
-            for key, value in kwargs.items():
-                if key == "id":
-                    if type(value) != int and value is not None:
-                        raise TypeError("id must be an integer")
-                    self.id = value
-                if key == "size":
-                    self.size = value
-                if key == "x":
-                    self.x = value
-                if key == "y":
-                    self.y = value
+        """update the attribute based on variable/keyword argument"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0: 
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,140 +1,64 @@
 #!/usr/bin/python3
+"""test for model/base.py"""
 import unittest
 from models.base import Base
-from models.square import Square
-import json
-import inspect
-
-'''
-    Creating test cases for the base module
-'''
 
 
-class test_base(unittest.TestCase):
-    '''
-        Testing base
-    '''
-    def test_id_none(self):
-        '''
-            Sending no id
-        '''
-        b = Base()
-        self.assertEqual(1, b.id)
+class TestBaseInstatiation(unittest.TestCase):
+    """Testing Base Instatiation"""
 
-    def test_id(self):
-        '''
-            Sending a valid id
-        '''
-        b = Base(50)
-        self.assertEqual(50, b.id)
+    def test_no_args(self):
+        """with no argument"""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_id_zero(self):
-        '''
-            Sending an id 0
-        '''
-        b = Base(0)
-        self.assertEqual(0, b.id)
+    def test_with_id(self):
+        """test with id"""
+        b1 = Base(12)
+        self.assertEqual(b1.id, Base(12).id)
 
-    def test_id_negative(self):
-        '''
-            Sending a negative id
-        '''
-        b = Base(-20)
-        self.assertEqual(-20, b.id)
+    def test_three_with_a_unique(self):
+        """test three with a unique id"""
+        b1 = Base()
+        b2 = Base(13)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+        self.assertNotEqual(b3.id, b2.id + 1)
 
-    def test_id_string(self):
-        '''
-            Sending an id that is not an int
-        '''
-        b = Base("Betty")
-        self.assertEqual("Betty", b.id)
+    def test_string_instance(self):
+        """test for type string instance"""
+        self.assertEqual("Hello", Base("Hello").id)
 
-    def test_id_list(self):
-        '''
-            Sending an id that is not an int
-        '''
-        b = Base([1, 2, 3])
-        self.assertEqual([1, 2, 3], b.id)
+    def test_get_instance(self):
+        """assurance that instance cannot be get"""
+        with self.assertRaises(AttributeError):
+            print(Base().__nb_instances)
 
-    def test_id_dict(self):
-        '''
-            Sending an id that is not an int
-        '''
-        b = Base({"id": 109})
-        self.assertEqual({"id": 109}, b.id)
+    def test_none_instance(self):
+        """test for None instance"""
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_id_tuple(self):
-        '''
-            Sending an id that is not an int
-        '''
-        b = Base((8,))
-        self.assertEqual((8,), b.id)
+    def test_for_float(self):
+        self.assertEqual(5.5, Base(5.5).id)
 
-    def test_to_json_type(self):
-        '''
-            Testing the json string
-        '''
-        sq = Square(1)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(type(json_string), str)
+    def test_for_list(self):
+        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
 
-    def test_to_json_value(self):
-        '''
-            Testing the json string
-        '''
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([json_dict])
-        self.assertEqual(json.loads(json_string),
-                         [{"id": 609, "y": 0, "size": 1, "x": 0}])
+    def test_for_dict(self):
+        self.assertEqual({'a': 2, 'b': 3}, Base({'a': 2, 'b': 3}).id)
 
-    def test_to_json_None(self):
-        '''
-            Testing the json string
-        '''
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string(None)
-        self.assertEqual(json_string, "[]")
+    def test_for_complex(self):
+        self.assertEqual(complex(5), Base(complex(5)).id)
 
-    def test_to_json_Empty(self):
-        '''
-            Testing the json string
-        '''
-        sq = Square(1, 0, 0, 609)
-        json_dict = sq.to_dictionary()
-        json_string = Base.to_json_string([])
-        self.assertEqual(json_string, "[]")
+    def test_for_range(self):
+        self.assertEqual(range(5), Base(range(5)).id)
 
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
 
-class TestSquare(unittest.TestCase):
-    """
-    class for testing Base class' methods
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        Set up class method for the doc tests
-        """
-        cls.setup = inspect.getmembers(Base, inspect.isfunction)
-
-    def test_module_docstring(self):
-        """
-        Tests if module docstring documentation exist
-        """
-        self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """
-        Tests if class docstring documentation exist
-        """
-        self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_func_docstrings(self):
-        """
-        Tests if methods docstring documntation exist
-        """
-        for func in self.setup:
-            self.assertTrue(len(func[1].__doc__) >= 1)
+if __name__ == "__main__":
+    unittest.main()
